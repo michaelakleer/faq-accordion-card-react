3e5923be36b03d045b79f7abff70e0bc276f1e0e
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-02-11 16:21:39 -0600

    Added information

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
 import React from "react";
+import WomanComputer from "./images/illustration-woman-online-desktop.svg";
+import Box from "./images/illustration-box-desktop.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="containerCard">
+          <div className="picture">
+            <img src={Box} alt="Box with @ symbol" className="box" />
+            <img
+              src={WomanComputer}
+              alt="Woman next to a computer"
+              className="womanComputer"
+            />
+          </div>
+          <div className="faq">
+            <h1>FAQ</h1>
+            <h2>
+              <a href="/">How many team members can I invite?</a>
+            </h2>
+            <h2>
+              <a href="/">What is the maximum file upload size?</a>
+            </h2>
+            <h2>
+              <a href="/">How do I reset my password?</a>
+            </h2>
+            <h2>
+              <a href="/">Can I cancel my subscription?</a>
+            </h2>
+            <h2>
+              <a href="/">Do you provide additional support?</a>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
