a0e9a68426e71d425e3eaedee8a3a01dcc287a6c
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-02-13 19:41:24 -0600

    Added classNames and another image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import WomanComputer from "./images/illustration-woman-online-desktop.svg";
 import Box from "./images/illustration-box-desktop.svg";
+import Down from "./images/icon-arrow-down.svg";
 import "./App.css";
 
 export default function App() {
@@ -18,21 +19,42 @@ export default function App() {
           </div>
           <div className="faq">
             <h1>FAQ</h1>
-            <h2>
-              <a href="/">How many team members can I invite?</a>
-            </h2>
-            <h2>
-              <a href="/">What is the maximum file upload size?</a>
-            </h2>
-            <h2>
-              <a href="/">How do I reset my password?</a>
-            </h2>
-            <h2>
-              <a href="/">Can I cancel my subscription?</a>
-            </h2>
-            <h2>
-              <a href="/">Do you provide additional support?</a>
-            </h2>
+            <div className="question">
+              <h2>
+                <a href="/">How many team members can I invite?</a>
+              </h2>
+              <img src={Down} alt="Down arrow" className="arrowDown" />
+            </div>
+
+            <div className="question">
+              <h2>
+                <a href="/">What is the maximum file upload size?</a>
+              </h2>
+              <img src={Down} alt="Down arrow" className="arrowDown" />
+            </div>
+
+            <div className="question">
+              <h2>
+                <a href="/">How do I reset my password?</a>
+              </h2>
+              <img src={Down} alt="Down arrow" className="arrowDown" />
+            </div>
+
+            <div className="question">
+              {" "}
+              <h2>
+                <a href="/">Can I cancel my subscription?</a>
+              </h2>
+              <img src={Down} alt="Down arrow" className="arrowDown" />
+            </div>
+
+            <div className="question">
+              {" "}
+              <h2>
+                <a href="/">Do you provide additional support?</a>
+              </h2>
+              <img src={Down} alt="Down arrow" className="arrowDown" />
+            </div>
           </div>
         </div>
       </div>
