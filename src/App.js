b1612249f32571e55ff16a46cd25c0385a8cbe53
michaelakleer <68608971+michaelakleer@users.noreply.github.com>
2021-02-15 16:28:40 -0600

    Deleted unnecessary div

--- src/App.js
+++ src/App.js
@@ -8,53 +8,51 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="containerCard">
-          <div className="picture">
-            <img
-              src={WomanComputer}
-              alt="Woman next to a computer"
-              className="womanComputer"
-            />
-            <img src={Box} alt="Box with @ symbol" className="box" />
+        <div className="picture">
+          <img
+            src={WomanComputer}
+            alt="Woman next to a computer"
+            className="womanComputer"
+          />
+          <img src={Box} alt="Box with @ symbol" className="box" />
+        </div>
+        <div className="faq">
+          <h1>FAQ</h1>
+          <div className="question">
+            <h2>
+              <a href="/">How many team members can I invite?</a>
+            </h2>
+            <img src={Down} alt="Down arrow" className="arrowDown" />
           </div>
-          <div className="faq">
-            <h1>FAQ</h1>
-            <div className="question">
-              <h2>
-                <a href="/">How many team members can I invite?</a>
-              </h2>
-              <img src={Down} alt="Down arrow" className="arrowDown" />
-            </div>
 
-            <div className="question">
-              <h2>
-                <a href="/">What is the maximum file upload size?</a>
-              </h2>
-              <img src={Down} alt="Down arrow" className="arrowDown" />
-            </div>
+          <div className="question">
+            <h2>
+              <a href="/">What is the maximum file upload size?</a>
+            </h2>
+            <img src={Down} alt="Down arrow" className="arrowDown" />
+          </div>
 
-            <div className="question">
-              <h2>
-                <a href="/">How do I reset my password?</a>
-              </h2>
-              <img src={Down} alt="Down arrow" className="arrowDown" />
-            </div>
+          <div className="question">
+            <h2>
+              <a href="/">How do I reset my password?</a>
+            </h2>
+            <img src={Down} alt="Down arrow" className="arrowDown" />
+          </div>
 
-            <div className="question">
-              {" "}
-              <h2>
-                <a href="/">Can I cancel my subscription?</a>
-              </h2>
-              <img src={Down} alt="Down arrow" className="arrowDown" />
-            </div>
+          <div className="question">
+            {" "}
+            <h2>
+              <a href="/">Can I cancel my subscription?</a>
+            </h2>
+            <img src={Down} alt="Down arrow" className="arrowDown" />
+          </div>
 
-            <div className="question">
-              {" "}
-              <h2>
-                <a href="/">Do you provide additional support?</a>
-              </h2>
-              <img src={Down} alt="Down arrow" className="arrowDown" />
-            </div>
+          <div className="question">
+            {" "}
+            <h2>
+              <a href="/">Do you provide additional support?</a>
+            </h2>
+            <img src={Down} alt="Down arrow" className="arrowDown" />
           </div>
         </div>
       </div>
